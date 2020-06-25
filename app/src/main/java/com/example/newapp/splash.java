803ae4052262c6997010e7cd85e6958ea954f20c
Vaibhav <vaibhavsam2511@gmail.com>
2020-06-25 20:49:20 +0530

    updated an error when no input was given

--- app/src/main/java/com/example/newapp/splash.java
+++ app/src/main/java/com/example/newapp/splash.java
@@ -15,9 +15,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class splash extends AppCompatActivity {
-    Animation top, bottom;
+    Animation top, bottom_right,bottom_left;
     ImageView im;
-    TextView t1;
+    TextView t1,t2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +25,23 @@ public class splash extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
         top = AnimationUtils.loadAnimation(this, R.anim.top_anim);
-        bottom = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+        bottom_right = AnimationUtils.loadAnimation(this, R.anim.bottom_right_anim);
+        bottom_left = AnimationUtils.loadAnimation(this, R.anim.bottom_left_anim);
         im = findViewById(R.id.logoimage);
         t1 = findViewById(R.id.text1);
+        t2=findViewById(R.id.text2);
 
         im.setAnimation(top);
-        t1.setAnimation(bottom);
+        t1.setAnimation(bottom_right);
+        t2.setAnimation(bottom_left);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(splash.this, login.class);
-                Pair[] pairs = new Pair[2];
+                Pair[] pairs = new Pair[3];
                 pairs[0] = new Pair<View, String>(im, "Logo");
                 pairs[1] = new Pair<View, String>(t1, "Logotext");
+                pairs[2]=new Pair<View,String>(t2,"Logotext2");
 
                 ActivityOptions activityoptions = ActivityOptions.makeSceneTransitionAnimation(splash.this, pairs);
 
